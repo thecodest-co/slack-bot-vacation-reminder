ba5f6ab89df58f01ba7ff9392a0abe79721ebece
Koziolek <bjkuczynski@gmail.com>
2022-09-21 10:08:02 +0200

    Add logger to runnigCalc provider

--- src/main/java/com/thecodest/slack/holidayreminder/RunningCalculatorProvider.java
+++ src/main/java/com/thecodest/slack/holidayreminder/RunningCalculatorProvider.java
@@ -2,12 +2,16 @@ package com.thecodest.slack.holidayreminder;
 
 import java.util.Optional;
 import javax.inject.Provider;
+import lombok.extern.java.Log;
 
+@Log
 class RunningCalculatorProvider implements Provider<RunningCalculator> {
 	@Override
 	public RunningCalculator get() {
 		return Optional.ofNullable(Boolean.valueOf(System.getenv("TEST_RUN")))
 				.or(() -> Optional.of(Boolean.FALSE))
+				.stream()
+				.peek(b -> log.warning("TEST_RUN is set to " + b))
 				.map(b -> b ? new FakeRunningCalculator() : new LocalRunningCalculator())
 				.get();
 	}
